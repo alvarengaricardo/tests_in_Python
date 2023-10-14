a086a2b2a3dc63241d33a736d9e2e79e584c2038
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-10-14 00:50:18 -0300

    functions to test - main + pytest

--- main.py
+++ main.py
@@ -122,7 +122,22 @@ except subprocess.CalledProcessError as e:
     print("Error:", e)
 
 # ----------------------------------------------------------------------------
-
+nome = "p_pytest.py"
+try:
+    print(f"Arquivo: {nome}")
+    arquivo.write(f"Arquivo: {nome}.\n")
+    start_time = time.time()
+    result = subprocess.run(["python", nome], capture_output=True, text=True, check=True)
+    # O resultado estará em result.stdout
+    #print("Output from program.py:", result.stdout)
+    end_time = time.time()
+    functions.now()
+    elapsed_time = end_time - start_time
+    print(f"Tempo gasto: {elapsed_time} segundos.")
+    arquivo.write(f"Tempo gasto: {elapsed_time} segundos.\n")
+    arquivo.write("------------------------------------------\n")
+except subprocess.CalledProcessError as e:
+    print("Error:", e)
 
 
 # ****************************************************************
