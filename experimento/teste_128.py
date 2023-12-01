8ef78b7216c5eb1c7211892320f8050703ab7565
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-11-30 23:48:27 -0300

    functions to test - nova versão multtestlib3

---
+++ experimento/teste_128.py
@@ -0,0 +1,34 @@
+import time
+from recursos.testes_pacotes import multtestlib3 as mtl3
+
+
+def slow_function(n):
+    time.sleep(n)
+    return n
+
+
+def teste(cpu, vetor):
+    start_time = time.time()
+    mtl3.test_equal(cpu, vetor, "", vetor, slow_function)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"CPUs: {cpu} - Tempo gasto: {elapsed_time} segundos.")
+
+
+def main():
+    vetor = [1] * 128
+    print(len(vetor))
+    teste(1, vetor)
+    teste(2, vetor)
+    teste(4, vetor)
+    teste(8, vetor)
+    teste(16, vetor)
+    teste(32, vetor)
+    teste(64, vetor)
+    teste(128, vetor)
+
+
+if __name__ == "__main__":
+    mtl3.init()
+    main()
+    mtl3.end()
