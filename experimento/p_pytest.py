08886e168973b9bd8f740e1fe51703751570e10b
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-12-06 16:20:00 -0300

    functions to test - nova versão multtestlib3

--- experimento/p_pytest.py
+++ experimento/p_pytest.py
@@ -1,5 +1,9 @@
+import time
 import functions
 import pytest
+
+start_time = time.time()
+
 
 # Use o decorador `@pytest.mark.parametrize` para parametrizar os testes
 @pytest.mark.parametrize("n", range(1, 101))
@@ -8,3 +12,7 @@ def test_slow_function(n):
         result = functions.slow_function(n)
         assert result == n
 
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Tempo gasto: {elapsed_time} segundos.")
